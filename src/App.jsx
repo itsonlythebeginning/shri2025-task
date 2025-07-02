91a58578bd0551aed57a656c003451949d425e96
Andrey <itsonlythebeginning@yandex.ru>
2025-07-02 04:46:23 +0300

    upload gh pages commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import Header from './components/Header';
-import Main from './components/Main';
-
-
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-
-  return(
+  return (
     <>
-      <Header/>
-      <Main/>
-
+      <Header />
+      <Main />
     </>
-  )
+  );
 }
 
 export default App;
